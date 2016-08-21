d2aa8826de44dad2d8d20b1e145c28f190305695
shanmdphd <pil.han@gmail.com>
2016-08-21 13:49:48 +0900

    update

--- rsconnect.R
+++ rsconnect.R
@@ -2,11 +2,7 @@ library(rsconnect)
 library(shiny)
 
 rsconnect::setAccountInfo(name='peetpass', token='24417CD3FA809B83AB17BD32C23EE26A', secret='S5mbbRFSRagDwJGwO36Wqje0fmcw5TTb6QvYH27E')
-deployApp() # This will upload files to rsconnect.
-deployApp(account='peetpass')
-#deployApp(account='wreckord')
-
-accounts(server = NULL)
-
-runApp()
-
+rsconnect::runApp()
+# This will upload files to rsconnect.
+rsconnect::deployApp(account='peetpass')
+# deployApp()
